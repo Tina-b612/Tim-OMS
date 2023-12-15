4bd64f7c3d538f46173543269d01dea5b351b037
“Jessica” <lyt_job@163.com>
2023-12-15 09:29:18 +0800

    添加index.html

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig(({ mode, command }) => {
         '/dev-api': {
           // target: 'http://localhost:8080',
           // target: 'http://192.168.0.123:8080',
-          target: 'http://192.168.0.119:82',
+          target: 'http://118.31.78.11:6001/',
           // target: 'http://10.0.0.4:8080',
           // target:'https://m323745n69.goho.co',
           changeOrigin: true,
